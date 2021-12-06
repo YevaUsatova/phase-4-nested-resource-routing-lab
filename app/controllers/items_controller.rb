963249e3d770e49c681c13b729d31cbb50b22a97
YevaUsatova <yeva.usatova@gmail.com>
2021-12-05 19:02:13 -0500

    commit tree c460c3fea1602ed3267b3a7c3d95365a2e947c38

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
+    if params[:user_id]
+      users = User.find(params[:user_id])
+      items = users.items
+    else
+      items = Item.all    
+    end
+    render json: items, include: :user  
+  end
+
+  def show
+    if params[:id]
+      items = Item.find(params[:id]) 
+    else
+      items = Item.all    
+    end
     render json: items, include: :user
+  end
+
+  def create
+    
+     items = Item.create(new_params) 
+     render json: items, include: :users, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Not found"}, status: :not_found
+  end
+  def new_params
+    params.permit[:name, :description, :price]
   end
 
 end
